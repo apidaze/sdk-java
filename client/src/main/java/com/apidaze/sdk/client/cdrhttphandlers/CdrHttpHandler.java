7c211fb775d45454ab8ac634c2fce68c0beebe79
Lucjan Skop <lucjan.skop@impaqgroup.com>
2019-10-31 13:33:48 +0100

    cdrhttphanlder format and callLeg as enum

--- client/src/main/java/com/apidaze/sdk/client/cdrhttphandlers/CdrHttpHandler.java
+++ client/src/main/java/com/apidaze/sdk/client/cdrhttphandlers/CdrHttpHandler.java
@@ -2,28 +2,30 @@ package com.apidaze.sdk.client.cdrhttphandlers;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Builder;
-import lombok.Value;
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.*;
 
 import java.time.ZonedDateTime;
+
+import static lombok.AccessLevel.PRIVATE;
 
 @Value
 @Builder
 public class CdrHttpHandler {
     Long id;
     String name;
-    String format; // TODO maybe enum?
+    Format format;
     String url;
-    String callLeg; // TODO maybe enum?
+    CallLeg callLeg;
     ZonedDateTime createdAt;
     ZonedDateTime updatedAt;
 
     @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
     public CdrHttpHandler(@JsonProperty("id") Long id,
                           @JsonProperty("name") String name,
-                          @JsonProperty("format") String format,
+                          @JsonProperty("format") Format format,
                           @JsonProperty("url") String url,
-                          @JsonProperty("call_leg") String callLeg,
+                          @JsonProperty("call_leg") CallLeg callLeg,
                           @JsonProperty("created_at") ZonedDateTime createdAt,
                           @JsonProperty("updated_at") ZonedDateTime updatedAt) {
         this.id = id;
@@ -34,4 +36,27 @@ public class CdrHttpHandler {
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
     }
+
+    @AllArgsConstructor(access = PRIVATE)
+    public enum CallLeg {
+        INBOUND("inbound"),
+        OUTBOUND("outbound");
+
+        @JsonValue
+        @Getter
+        private final String value;
+    }
+
+    @AllArgsConstructor(access = PRIVATE)
+    public enum Format {
+        REGULAR("regular"),
+        CSV("csv"),
+        XML("xml"),
+        JSON("json");
+
+        @JsonValue
+        @Getter
+        private final String value;
+    }
+
 }
