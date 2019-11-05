d30b357ea8b6d8aa867c981e6ee45d72794f8b02
Lucjan Skop <lucjan.skop@impaqgroup.com>
2019-11-05 13:20:30 +0100

    create CdrHttpHandler example

--- examples/src/main/java/com/apidaze/sdk/examples/cdrhttphandlers/CreateCdrHttpHandlerExample.java
+++ examples/src/main/java/com/apidaze/sdk/examples/cdrhttphandlers/CreateCdrHttpHandlerExample.java
@@ -1,4 +1,4 @@
-package com.apidaze.sdk.examples.externalscripts;
+package com.apidaze.sdk.examples.cdrhttphandlers;
 
 import com.apidaze.sdk.client.ApplicationAction;
 import com.apidaze.sdk.client.base.Credentials;
@@ -13,7 +13,7 @@ import java.io.IOException;
 import static java.util.Objects.isNull;
 
 @Slf4j
-public class CreateExternalScript {
+public class CreateCdrHttpHandlerExample {
 
     public static void main(String... args) {
 
@@ -25,22 +25,22 @@ public class CreateExternalScript {
             System.exit(1);
         }
 
-        val scriptName = "Some cool script";
-        val scriptUrl = "http://cool.script.com";
+        val handlerName = "Some cool handler";
+        val handlerUrl = "http://cool.handler.com";
 
         // initiate ApplicationAction
         val applicationAction = ApplicationAction.create(new Credentials(apiKey, apiSecret));
 
         try {
-            // create an external script
-            val createdScript = applicationAction.createExternalScript(scriptName, URL.fromString(scriptUrl));
-            log.info("Created {}", createdScript);
+            // create a new CdrHttpHandler
+            val result = applicationAction.createCdrHttpHandler(handlerName, URL.fromString(handlerUrl));
+            log.info("Created CdrHttpHandler: {}", result);
         } catch (HttpResponseException e) {
             log.error(e.toString());
         } catch (IOException e) {
             log.error("An IO error occurred during communicating with API", e);
         } catch (InvalidURLException e) {
-            log.error("scriptUrl is invalid ", e);
+            log.error("handlerUrl is invalid ", e);
         }
     }
 }
