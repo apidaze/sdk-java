d3c389ff343267c58b9f4af620ba0bef0e62f870
Lucjan Skop <lucjan.skop@impaqgroup.com>
2019-11-26 13:55:03 +0100

    applications java doc

--- client/src/main/java/com/apidaze/sdk/client/applications/Applications.java
+++ client/src/main/java/com/apidaze/sdk/client/applications/Applications.java
@@ -1,5 +1,7 @@
 package com.apidaze.sdk.client.applications;
 
+import com.apidaze.sdk.client.http.HttpResponseException;
+
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
@@ -8,7 +10,30 @@ public interface Applications {
 
     List<Application> getApplications() throws IOException;
 
+    /**
+     * Returns an application details retrieved by application id.
+     * @param id id of the application to fetch
+     * @return an application details, otherwise {@code Optional.empty()}
+     * @throws IOException
+     * @throws HttpResponseException
+     */
     Optional<Application> getApplicationById(Long id) throws IOException;
+
+    /**
+     * Returns an application details retrieved by api key.
+     * @param apiKey api key of the application to fetch
+     * @return an application details, otherwise {@code Optional.empty()}
+     * @throws IOException
+     * @throws HttpResponseException
+     */
     Optional<Application> getApplicationByApiKey(String apiKey) throws IOException;
+
+    /**
+     * Returns an application details retrieved by api key.
+     * @param name the name of the application to fetch
+     * @return an application details, otherwise {@code Optional.empty()}
+     * @throws IOException
+     * @throws HttpResponseException
+     */
     Optional<Application> getApplicationByName(String name) throws IOException;
 }
